2ea217df2990d1e345519f5a3da7fb202dcadb71
sarasteinsland <sarasteinsland01@gmail.com>
2025-12-10 11:09:15 +0100

    fikset tittel i header

--- ecoVekt/app/(tabs)/admin/profile.tsx
+++ ecoVekt/app/(tabs)/admin/profile.tsx
@@ -483,7 +483,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   headerTitle: {
-    fontSize: 35,
+    fontSize: 25,
     fontWeight: "700",
     color: colors.background, 
   },
